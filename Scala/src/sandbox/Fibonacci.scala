9caa3e92c22073e41e340b67ac35b879724fcebf
Timothy Savard <tpsavard@gmail.com>
2016-10-08 11:05:57 -0700

    Removed non-working solution. Made a Scala example more sensible.

--- Scala/src/sandbox/Fibonacci.scala
+++ Scala/src/sandbox/Fibonacci.scala
@@ -4,15 +4,15 @@ package sandbox
   * Created by Tim on 3/8/16.
   */
 object Fibonacci extends App {
-  def fibonacci(threshold: Int) = {
-    def fibInner(x: Int, y: Int): Unit = {
-      val z = x + y
-      if (z <= threshold) {
+  def fibonacci(threshold: BigInt) = {
+    def fibInner(x: BigInt, y: BigInt, step: Int): Unit = {
+      if (step <= threshold) {
+        val z = x + y
         println(z)
-        fibInner(y, z)
+        fibInner(y, z, step + 1)
       }
     }
-    fibInner(0, 1)
+    fibInner(0, 1, 2)
   }
-  fibonacci(100)
+  fibonacci(25)
 }
