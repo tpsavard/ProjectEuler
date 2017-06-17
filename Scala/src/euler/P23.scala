db2e74bad28ebdcff3ac15c7394becf8846fc588
Timothy Savard <tpsavard@gmail.com>
2017-06-17 15:35:30 -0700

    Solved problem 23.

--- Scala/src/euler/P23.scala
+++ Scala/src/euler/P23.scala
@@ -5,14 +5,14 @@ package euler
   */
 object P23 extends App {
 
-  // Get the list of all abundant numbers from 1 to 28123
+  // Get the list of all abundant numbers in the given range
   def getAbundantNumbers(numbers: List[Int], abundantNumbers: List[Int]): List[Int] = numbers match {
     case Nil => abundantNumbers
     case head :: tail =>
       if (isAbundantNumber(head)) {
-        return getAbundantNumbers(tail, head :: abundantNumbers)
+        getAbundantNumbers(tail, head :: abundantNumbers)
       } else {
-        return getAbundantNumbers(tail, abundantNumbers)
+        getAbundantNumbers(tail, abundantNumbers)
       }
   }
 
@@ -22,25 +22,43 @@ object P23 extends App {
     }
 
     var d = List(1)
-    for (i <- 2 to (number / 2f).round) {
+    for (i <- 2 to (number.toFloat / 2).toInt) {
       if (number % i == 0) {
         d = i :: d
       }
     }
 
-    return d.sum == number
+    d.sum > number
   }
 
   // Get the list of all sums of two abundant numbers
+  def collectSums(numbers: List[Int], sums: List[Int]): List[Int] = numbers match {
+    case Nil => sums
+    case head :: tail =>
+      collectSums(tail, sumNumbers(head, tail, List()) ::: sums)
+  }
 
-  // Remove abundant sums from all integers 1 to 28123
-
-  // Sum remaining integers
+  def sumNumbers(number: Int, numbers: List[Int], sums: List[Int]): List[Int] = numbers match {
+    case Nil => (number + number) :: sums
+    case head :: tail =>
+      sumNumbers(number, tail, (number + head) :: sums)
+  }
 
   // ~
 
-  //val anums = getAbundantNumbers((1 to 28123).toList, List())
-  val anums = getAbundantNumbers((1 to 20).toList, List())
-  println(anums)
+  // Problem range: 1 to 28123
+  val upperLimit = 28123
 
+  print("Getting abundant numbers...")
+  val anums = getAbundantNumbers((1 to upperLimit).toList, List())
+  println("Found " + anums.size)
+
+  print("Collecting abundant sums...")
+  val asums = collectSums(anums, List())
+  println("Found " + asums.size)
+
+  val nonAsums = (1 to upperLimit).toList diff asums
+  val ans = nonAsums.sum
+
+  println(ans)
 }
