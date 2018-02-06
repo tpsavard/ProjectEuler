2ce96b07b00549e51c5021798f7e76ed03912dc5
Timothy Savard <timsav@amazon.com>
2018-02-06 10:01:42 -0800

    Reorganizing solution.

--- Scala/src/euler/P26.scala
+++ Scala/src/euler/P26.scala
@@ -6,10 +6,19 @@ import scala.math.BigDecimal.RoundingMode
 
 object P26 extends App {
 
-    val dividend: BigDecimal = BigDecimal(1, new MathContext(s"precision=50 roundingMode=FLOOR"))
-    val divisor: BigDecimal = BigDecimal(7)
-    val result: BigDecimal = dividend./(divisor)
+    def getLongestRecurringCycle(fraction: BigDecimal): String = {
+        val output: String = "%.1024f".format(fraction)
+        output
+    }
 
-    val test: String = "%.50f".format(result)
-    println(test)
+    val dividend: BigDecimal = BigDecimal(1, new MathContext("precision=1024 roundingMode=FLOOR"))
+    for (i <- 2 to 1000) {
+        // Get the longest recurring cycle
+        val divisor: BigDecimal = BigDecimal(i)
+        val output: String = getLongestRecurringCycle(dividend./(divisor))
+
+        // Compare cycle length
+        println(s"$divisor, $output")
+    }
+
 }
