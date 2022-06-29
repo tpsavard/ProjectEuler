8a15d3823676e323729de45518caf4652a3a069f
tpsavard <tpsavard@gmail.com>
2022-06-28 18:34:54 -0700

    Added Rust implementation for p35

---
+++ rust/src/main.rs
@@ -0,0 +1,6 @@
+mod utils;
+mod problems;
+
+fn main() {
+    println!("p35 answer: {}", crate::problems::p35::p35());
+}
---
+++ rust/src/problems/p35.rs
@@ -0,0 +1,23 @@
+/// Project Euler, problem #35
+/// > The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+/// > There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+/// > How many circular primes are there below one million?
+pub fn p35() -> i32 {
+    let mut count = 0;
+    for n in crate::utils::generate_primes(1000000) {
+        if is_circular(n) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn is_circular(n: i32) -> bool {
+    let rotated_values = crate::utils::get_rotated_values(n);
+    for rotated_value in rotated_values {
+        if !crate::utils::is_prime(rotated_value) {
+            return false;
+        }
+    }
+    return true;
+}
