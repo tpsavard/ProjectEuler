103e532ca1fdcc4e534d1fabd162d31bb6addb4b
tpsavard <tpsavard@gmail.com>
2022-07-10 00:55:27 -0700

    Added p36.

--- rust/src/problems/p35.rs
+++ rust/src/problems/p35.rs
@@ -2,14 +2,15 @@
 /// > The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
 /// > There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
 /// > How many circular primes are there below one million?
-pub fn p35() -> i32 {
+
+pub fn p35() -> String {
     let mut count = 0;
-    for n in crate::utils::generate_primes(1000000) {
+    for n in crate::utils::generate_primes(1_000_000) {
         if is_circular(n) {
             count += 1;
         }
     }
-    return count;
+    return count.to_string();
 }
 
 fn is_circular(n: i32) -> bool {
